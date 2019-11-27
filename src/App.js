833e174952c342be43ca6c3e479188b4c4843a25
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2019-11-27 23:51:18 +0200

    multiple paylists

--- src/App.js
+++ src/App.js
@@ -3,29 +3,58 @@ import SongList from './components/SongList.js';
 import PlaylistList from './components/PlaylistList';
 
 function App() {
-  return (
-    <div className="App">
+    let playlistSongs = [
+        {
+            "playlistId": 1,
+            "songs" : [
+                {name: 'Hey Big Eyes',
+                    album: 'Pang',
+                    artist: 'Caroline Polachek',
+                    length: '3:01',
+                    year: 2019},
+                {name: 'LesAlpx',
+                    album: 'Crush',
+                    artist: 'Floating Points',
+                    length: '5:13',
+                    year: 1999 }
+            ]
+        },
+        {
+            "playlistId": 2,
+            "songs" : [
+                {name: 'Bess´s Dance',
+                    album: 'Spring',
+                    artist: 'Itasca',
+                    length: '4:05',
+                    year: 2019},
+                {name: 'Conscious',
+                    album: 'Ocean Blues',
+                    artist: 'Yula Kasp',
+                    length: '4:42',
+                    year: 2016 }
+            ]
+        }
+    ];
+
+    let playlists = [
+        {
+            "id" : 1,
+            "name" : "Uudet tsekattavat"
+        },
+        {
+            "id" : 2,
+            "name" : "Vanhat tsekattavat"
+        }
+    ];
+
+    let songs = playlistSongs.find(playlist => playlist.playlistId === 2).songs;
+
+    return <div className="App">
         <PlaylistList playlists={playlists}/>
         <SongList songs={songs}/>
-    </div>
-  );
+    </div>;
 }
 
-let songs = [
-  {name: 'Hey Big Eyes',
-    album: 'Pang',
-    artist: 'Caroline Polachek',
-    length: '3:01',
-    year: 2019},
-  {name: 'LesAlpx',
-    album: 'Crush',
-    artist: 'Floating Points',
-    length: '5:13',
-    year: 1999 }
-];
 
-let playlists = [
-    {"name" : "Uudet tsekattavat"}
-];
 
 export default App;
--- src/components/PlaylistList.js
+++ src/components/PlaylistList.js
@@ -5,7 +5,7 @@ function PlaylistList(props) {
         <h2>Playlists</h2>
         <ul>
         {props.playlists.map((playlist) =>
-            <li>{playlist.name}</li>
+            <li key={playlist.id}>{playlist.name}</li>
         )}
     </ul>
     </div>
