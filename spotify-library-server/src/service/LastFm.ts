b2adc4b57829ebbeddac166c1883947d960d9bb8
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-01-12 22:33:50 +0200

    Server: Fixed namespace to match TypeScript

--- spotify-library-server/src/service/LastFm.ts
+++ spotify-library-server/src/service/LastFm.ts
@@ -1,9 +1,10 @@
 import {TrackContext} from "../model/context";
+import * as LastFm from "../model/external/lastFm";
 const fetch = require('node-fetch');
 
 const apiKey = 'b89e9ec47e543b6b47adeef5a0341118';
 
-export function searchTrack(context : TrackContext) : Promise<lastFm.Track> {
+export function searchTrack(context : TrackContext) : Promise<LastFm.Track> {
     return fetch("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key="+apiKey+"&artist="+context.artists[0].name+"&track="+context.spotifyTrack.name+"&format=json&user=pipo9")
         .then(response =>
             response.json())
