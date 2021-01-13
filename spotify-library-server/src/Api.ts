7d6ab3ef1fcda900a5ab1936c9b5f0317abd54c3
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-01-14 00:16:31 +0200

    Server: Renamed Playlist to Library

--- spotify-library-server/src/Api.ts
+++ spotify-library-server/src/Api.ts
@@ -1,18 +1,18 @@
 import express = require("express");
 import cors = require('cors')
-import * as Playlist from "./Playlist"
+import * as Library from "./Library"
 
 const app = express();
 app.use(cors())
 const port = 3001;
 
 app.get('/user-playlists', (req, res) => {
-    Playlist.getUserPlaylists(req.header('authorization'))
+    Library.getUserPlaylists(req.header('authorization'))
         .then(playlists => res.send(playlists));
 })
 
 app.get('/playlist/:playlistId', (req, res) => {
-    Playlist.getPlaylist(req.params.playlistId).then(
+    Library.getPlaylist(req.params.playlistId).then(
         playlist => res.send(playlist)
     );
 });
