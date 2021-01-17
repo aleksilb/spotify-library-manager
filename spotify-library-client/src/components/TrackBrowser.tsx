8178baa1ce3575af4c4d3053366930e3b805a953
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-01-18 00:24:29 +0200

    Client: Track browser to use sortable data table

--- spotify-library-client/src/components/TrackBrowser.tsx
+++ spotify-library-client/src/components/TrackBrowser.tsx
@@ -1,40 +1,26 @@
 import * as React from "react";
-import {Track} from "../model/interfaces";
+import {Album, Artist, Track} from "../model/interfaces";
+import {ColDef, DataGrid, ValueGetterParams} from "@material-ui/data-grid";
 
 interface TrackBrowserProps {
     tracks ?: Track[];
 }
 
+const columns: ColDef[] = [
+    { field: 'name', headerName: 'Name', width: 200 },
+    { field: 'artists', headerName: 'Artists', width: 200,
+        valueFormatter: (params: ValueGetterParams) => {
+            let artists = params.value as Artist[];
+            let artistNames = artists != null ? artists.map((artist) => artist.name) : [];
+            return artistNames.join(', ')}},
+    { field: 'album', headerName: 'Album', width: 200, valueFormatter: params => (params.value as Album).name },
+    { field: 'year', headerName: 'Year', width: 100 },
+    { field: 'plays', headerName: 'Plays', width: 100 },
+]
+
 export const TrackBrowser = ({tracks} : TrackBrowserProps) => {
-    return <div>
+    return <div style={{ height: 1200, width: '100%' }} >
         <h2>Playlist</h2>
-        <table>
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>Artists</th>
-            <th>Album</th>
-            <th>Year</th>
-            <th>Country</th>
-            <th>Plays</th>
-        </tr>
-        </thead>
-        <tbody>
-        {tracks != null && tracks.map((track) => {
-            let artistNames = track.artists != null ? track.artists.map((artist) => {
-                return artist.name;
-            }) : [];
-            return <tr key={track.id}>
-                <td>{track.name}</td>
-                <td>{artistNames.join(', ')}</td>
-                <td>{track.album != null && track.album.name}</td>
-                <td>{track.year}</td>
-                <td>{track.country}</td>
-                <td>{track.plays}</td>
-            </tr>
-            }
-        )}
-        </tbody>
-    </table>
+        {tracks != null && <DataGrid columns={columns} rows={tracks} />}
     </div>
 };
