9e78da1c9a5e9564862da433a5035ddb99b332d7
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-01-17 16:50:05 +0200

    Server: Disabling MusicBrainz data fetching

--- spotify-library-server/src/Services.ts
+++ spotify-library-server/src/Services.ts
@@ -4,6 +4,8 @@ import * as Mapping from "./Mapping";
 import * as LastFm from "./service/LastFm";
 import {AlbumContext, ArtistContext, TrackContext} from "./model/context";
 
+const MUSIC_BRAINZ_ENABLED = false;
+
 export function getAlbum(context: AlbumContext): Promise<Album> {
     return Promise.resolve(Mapping.createAlbum(context));
 }
@@ -16,10 +18,10 @@ export function getTrack(context: TrackContext): Promise<Track> {
         });
 }
 
-export function getArtist(context: ArtistContext): Promise<Artist> {
-    return MusicBrainz.searchArtist(context.spotifyArtist)
-        .then(mbArtist => {
-            context.musicBrainzArtist = mbArtist;
-            return Mapping.createArtist(context);
-        });
+export async function getArtist(context: ArtistContext): Promise<Artist> {
+    if(MUSIC_BRAINZ_ENABLED) {
+        let mbArtist = await MusicBrainz.searchArtist(context.spotifyArtist);
+        context.musicBrainzArtist = mbArtist;
+    }
+    return Mapping.createArtist(context);
 }
